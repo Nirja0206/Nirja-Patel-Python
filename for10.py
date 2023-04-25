d402d6aacdf1667703a0746477a6deae34281a5d
Nirja0206 <nirjajpatel1712@gmail.com>
2023-04-25 13:49:30 +0530

    first python programs

---
+++ for10.py
@@ -0,0 +1,4 @@
+for i in range(1,7):
+    print(" "*(6-i),"*"*i)
+for i in range(1,7):
+    print(" "*i,"*"*(6-i))
---
+++ for11.py
@@ -0,0 +1,4 @@
+for i in range(1,6):
+    print(" "*(6-i)," *"*i)
+for j in range(1,5):
+    print(" "*(j+1)," *"*(5-j))
